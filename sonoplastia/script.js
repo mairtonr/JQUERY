d1b8680fd959d17866c6bf6d0fc50c3bf5cd13bf
mairtonr <mairtonhotmailcomr44@gmail.com>
2022-06-17 14:31:51 -0300

    Sonoplastia

---
+++ sonoplastia/script.js
@@ -0,0 +1,43 @@
+$(document).ready(()=>{
+    $("#rapaz").click(function () {
+        var rapaz = new Audio("sons/rapaz.mp3")
+        rapaz.play()
+    })
+
+    $("#calma").click(()=>{
+        var calma = new Audio("sons/calma.mp3")
+        calma.play()
+    })
+    $("#uepa").click(()=>{
+        var uepa = new Audio("sons/uepa.mp3")
+        uepa.play()
+    })
+    $("#elegosta").click(()=>{
+        var elegosta = new Audio("sons/elegosta.mp3")
+        elegosta.play()
+    })
+    $("#olha").click(()=>{
+        var olha = new Audio("sons/olha.mp3")
+        olha.play()
+    })
+    $("#danca").click(()=>{
+        var danca = new Audio("sons/gatinhodanca.mp3")
+        danca.play()
+    })
+    $("#ui").click(()=>{
+        var ui = new Audio("sons/ui.mp3")
+        ui.play()
+    })
+    $("#tome").click(()=>{
+        var tome = new Audio("sons/tome.mp3")
+        tome.play()
+    })
+    $("#cavalo").click(()=>{
+        var cavalo = new Audio("sons/cavalo.mp3")
+        cavalo.play()
+    })
+    $("#papelao").click(()=>{
+        var papelao = new Audio("sons/papelao.mp3")
+        papelao.play()
+    })
+})
